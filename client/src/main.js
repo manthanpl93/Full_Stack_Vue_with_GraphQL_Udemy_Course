c359f465664c057ffe6686ebc44e247f82ce2747
Manthan Patel <manthanpl93@live.com>
2021-02-11 19:16:17 +0530

    Configure VueApollo

--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,22 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
 
 Vue.config.productionTip = false
 
+Vue.use(VueApollo);
+
+const defaultClient = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const apolloProvider = new VueApollo({ defaultClient });
+
+
 new Vue({
+  provide: apolloProvider.provide(),
   router,
   store,
   vuetify,
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,75 @@
 <template>
   <div class="home">
-    <h3>Home</h3>
+    <v-container text-xs-center v-if="getPosts">
+      <v-flex xs12>
+        <v-carousel v-bind="{ cycle: true }" interval="3000">
+          <v-carousel-item
+            v-for="post in getPosts"
+            :key="post._id"
+            :src="post.imageUrl"
+          >
+            <h1 id="carousel__title">{{ post.title }}</h1>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-container>
   </div>
 </template>
 
 <script>
+import { gql } from "apollo-boost";
 export default {
-  name: 'Home'
-}
+  name: "Home",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            categories
+            description
+            createdDate
+            likes
+            createdBy {
+              _id
+              username
+              email
+              password
+              joinDate
+            }
+          }
+        }
+      `,
+      result({ data, loading }) {
+        if (!loading) {
+          this.posts = data.getPosts();
+        }
+      },
+      error(err) {
+        console.error(err);
+      },
+    },
+  },
+};
 </script>
+
+<style scoped>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
